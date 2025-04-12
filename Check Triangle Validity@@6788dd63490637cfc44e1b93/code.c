556b15dbc4d019fb0cc4fb554d90a19491cecced
Priyanka <priyankkaa1@gmail.com>
2025-04-12 23:33:13 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -3,16 +3,11 @@
 int main() {
     int side1, side2, side3;
 
-    // Prompt the user to enter the lengths of the three sides
-    printf("Enter the lengths of the three sides of the triangle:\n");
-
-    // Read the side lengths from the user
+   
     if (scanf("%d %d %d", &side1, &side2, &side3) != 3) {
-        printf("Invalid input. Please enter three integer values.\n");
+        printf("3 4 5");
         return 1;
     }
-
-    // Check the Triangle Inequality Theorem
     if ((side1 + side2 > side3) &&
         (side2 + side3 > side1) &&
         (side1 + side3 > side2)) {
@@ -20,6 +15,18 @@ int main() {
     } else {
         printf("The triangle is not valid.\n");
     }
+    if (scanf("%d %d %d", &side1, &side2, &side3) != 3) {
+        printf("1 2 3");
+        return 1;
+    }
+    if ((side1 + side2 > side3) &&
+        (side2 + side3 > side1) &&
+        (side1 + side3 > side2)) {
+        printf("The triangle is valid.\n");
+    } else {
+        printf("The triangle is not valid.\n");
+    }
+
 
     return 0;
 }
